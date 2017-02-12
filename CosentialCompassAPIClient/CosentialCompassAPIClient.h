a746e8af2d1cb14e27aecf73f69fdd0101269fa7
SoftDev0420 <softdev0420@gmail.com>
2017-02-12 21:33:32 +0100

    Create a project

---
+++ CosentialCompassAPIClient/CosentialCompassAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  CosentialCompassAPIClient.h
+//  CosentialCompassAPIClient
+//
+//  Created by SoftDev on 2/11/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CosentialCompassAPIClient.
+FOUNDATION_EXPORT double CosentialCompassAPIClientVersionNumber;
+
+//! Project version string for CosentialCompassAPIClient.
+FOUNDATION_EXPORT const unsigned char CosentialCompassAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CosentialCompassAPIClient/PublicHeader.h>
+
+
